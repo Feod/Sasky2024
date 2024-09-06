6c97d93cd8c21f718a16404d1751c3ffc40a5491
papujussi <63768385+papujussi@users.noreply.github.com>
2024-09-06 07:38:21 +0300

    Add files via upload

---
+++ VaihtoehtoinenRyhmätyökansio_VESA/projektia;.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Heipa Miikka! Mä olen koittanut keksiä jotai mutta pää on ihan tyhjä!");
+    }
+}
