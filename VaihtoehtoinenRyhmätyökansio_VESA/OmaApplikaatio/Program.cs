ae1f73f70c1f43da794f6837c760be08e4ed105b
ThinkPadBoi <miikkaharjuntausta@gmail.com>
2024-09-06 10:49:14 +0300

    Tein C#-projektikansion ja siirsin koodin Program.cs tiedostoon ja jatkoin sitä hieman

---
+++ VaihtoehtoinenRyhmätyökansio_VESA/OmaApplikaatio/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Heipa Miikka! Mä olen koittanut keksiä jotai mutta pää on ihan tyhjä!");
+        Thread.Sleep(1000);
+        Console.WriteLine("----");
+        Thread.Sleep(1000);
+        Console.WriteLine("Moro Vesa! Ei se mitään. Kokeillaan tehdä kuitenkin jokin toiminnallisuus tänne.");
+        Thread.Sleep(1000);
+        Console.WriteLine("----");
+        Thread.Sleep(1000);
+
+        //Oikea toiminnallisuus alkaa tästä:
+
+        Console.WriteLine("Valitse toiminnallisuus");
+        Console.WriteLine("1: Heitä noppaa. 2: Generoi satunnainen nimi");
+
+
+        string vastaus = Console.ReadLine();
+        int vastaus_int = 0;
+        int.TryParse(vastaus, out vastaus_int);
+
+        if (vastaus_int == 1)
+        {
+            Console.WriteLine("Valitsit nopan heiton");
+            //TODO: Nopan heitto logiikka
+        }
+        else if (vastaus_int == 2)
+        {
+            Console.WriteLine("Valitsit nimen");
+            //TODO: Nimi logiikka
+        }
+        else
+        {
+            Console.WriteLine("En ymmärtänyt vastaustasi");
+        }
+
+
+
+        Thread.Sleep(1000);
+        Console.WriteLine("Ohjelma on päättynyt");
+
+
+    }
+}
--- VaihtoehtoinenRyhmätyökansio_VESA/projektia;.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Heipa Miikka! Mä olen koittanut keksiä jotai mutta pää on ihan tyhjä!");
-    }
-}
